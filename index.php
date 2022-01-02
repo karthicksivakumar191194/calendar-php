266c74bb9fa373bf0186a37fe7f4295d27c944a9
Karthick Sivakumar <karthicksivakumardev@gmail.com>
2022-01-03 00:39:31 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+  ini_set('default_charset', 'UTF-8');
+  date_default_timezone_set('Europe/Rome');
+  $lang = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? substr($_SERVER['HTTP_ACCEPT_LANGUAGE'],0,2) : 'en';
+?>
+<!DOCTYPE html>
+
+<html lang="<?php echo $lang; ?>">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Php Calendar</title>
+  <link rel="stylesheet" type="text/css" media="screen" href="calendar.css">
+</head>
+<body>
+<?php
+  if (file_exists(realpath("calendar_$lang.php"))) {
+    include("calendar_$lang.php");
+  } else {
+    echo "No Calendar to View";
+  }
+?>
+</body>
+</html>
